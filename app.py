e97295c8c1e5e9fca8767fa4da5531f309ee3e5c
Dianga Michael <diangamichael@gmail.com>
2023-09-02 02:02:58 -0700

    Update app.py
    add aai

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import os
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
+import numpy as np
 
 app = Flask(__name__)
 
@@ -9,9 +12,28 @@ ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'gif'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# Load a pre-trained CNN model (VGG16 in this example)
+model = VGG16(weights='imagenet')
+
 # Function to check if the file has an allowed extension
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# Function to process the uploaded image and get suggestions
+def analyze_soil_image(file):
+    img = image.load_img(file, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    
+    # Get the CNN predictions for the image
+    predictions = model.predict(x)
+    decoded_predictions = decode_predictions(predictions, top=3)[0]
+    
+    # Generate suggestions based on the predictions (replace with your generative AI code)
+    suggestions = [f"{label}: {score:.2f}" for _, label, score in decoded_predictions]
+    
+    return suggestions
 
 @app.route('/')
 def index():
@@ -32,12 +54,13 @@ def upload_file():
         filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(filename)
 
-        # Process the uploaded image and get suggestions here
-        # You can call your AI models and generate suggestions
+        # Analyze the uploaded image and get suggestions
+        suggestions = analyze_soil_image(filename)
 
         # Display the suggestions on a new page or return them as JSON
+        return render_template('suggestions.html', suggestions=suggestions)
 
-        return "File successfully uploaded and analyzed"
+    return "Invalid file format"
 
 if __name__ == '__main__':
     app.run(debug=True)
